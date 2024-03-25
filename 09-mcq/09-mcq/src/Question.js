f95e93c86a2f13d5bed6b2e7d44fae99e965b1e0
lurbh <53887968+lurbh@users.noreply.github.com>
2024-03-25 12:08:38 +0800

    Mcq completed

--- 09-mcq/09-mcq/src/Question.js
+++ 09-mcq/09-mcq/src/Question.js
@@ -4,21 +4,26 @@ export default function Question() {
 
     const [answer, setAnswer] = useState("");
 
+    const updateAnswer = (e) =>{
+      console.log(e);
+      setAnswer(e.target.value);
+    }
+
     return (
         <section id="qns1">
         <h1>Question 1: Which island is Mount Faber, Singapore, located on?</h1>
         <ul>
           <li>
-            <input name="qns1answer" type="radio" value="A"/><label>A. Palau Ubin</label>
+            <input name="qns1answer" type="radio" value="A" onChange={updateAnswer} checked={answer==="A"}/><label>A. Palau Ubin</label>
           </li>
           <li>
-            <input name="qns1answer" type="radio" value="B"/><label>B. Palau Ujong</label>
+            <input name="qns1answer" type="radio" value="B" onChange={updateAnswer} checked={answer==="B"}/><label>B. Palau Ujong</label>
           </li>
           <li>
-            <input name="qns1answer" type="radio" value="C"/><label>C. Palau Tekong</label>
+            <input name="qns1answer" type="radio" value="C" onChange={updateAnswer} checked={answer==="C"}/><label>C. Palau Tekong</label>
           </li>
           <li>
-            <input name="qns1answer" type="radio" value="D"/><label>D. Saint John Island</label>
+            <input name="qns1answer" type="radio" value="D" onChange={updateAnswer} checked={answer==="D"}/><label>D. Saint John Island</label>
           </li>
         </ul>
       </section>

ad6a1f68bae39f8e4454653d40fa4fdd15b7e9bc
lurbh <53887968+lurbh@users.noreply.github.com>
2024-03-25 12:41:17 +0800

    Buffet completed

--- 10-buffet/10-buffet/src/Buffet.js
+++ 10-buffet/10-buffet/src/Buffet.js
@@ -1,29 +1,57 @@
 import React, {useState} from 'react'
 
+const itemPrices= {
+  "beef" : 12.00,
+  "chicken" : 7.50,
+  "fish" : 10.00,
+  "mushrooms" : 5.50,
+  "spinach" : 2.50
+}
+
 export default function Buffet() {
 
     const [items, setItems] = useState([]);
 
     const calculateTotal = () => {
-        return 0;
+      let total = 0;
+      for(let i of items)
+      {
+        total += itemPrices[i];
+      }
+      return total;
+    }
+
+    const handleBoxChange = (e) => {
+      const { value, checked } = e.target;
+      if (checked)
+        if(items.length  > 0)
+          setItems([...items, value]);
+        else
+          setItems([value]);
+      else
+      {
+        let foodarray = items.filter((item) => item !== value)
+        setItems(foodarray);
+      }
+
     }
 
     return (
       <React.Fragment>
           <div>
-            <input type="checkbox" name="orders" value="beef"/><label>Shabu Shabu Beef</label>
+            <input type="checkbox" name="orders" value="beef" onChange={handleBoxChange} checked={items.includes('beef')}/><label>Shabu Shabu Beef</label>
           </div>
           <div>
-            <input type="checkbox" name="orders" value="chicken"/><label>Chicken Fillet</label>
+            <input type="checkbox" name="orders" value="chicken" onChange={handleBoxChange} checked={items.includes('chicken')}/><label>Chicken Fillet</label>
           </div>
           <div>
-            <input type="checkbox" name="orders" value="fish"/><label>Fish Fillet</label>
+            <input type="checkbox" name="orders" value="fish" onChange={handleBoxChange} checked={items.includes('fish')}/><label>Fish Fillet</label>
           </div>
           <div>
-            <input type="checkbox" name="orders" value="mushrooms"/><label>Assorted mushhrooms</label>
+            <input type="checkbox" name="orders" value="mushrooms" onChange={handleBoxChange} checked={items.includes('mushrooms')}/><label>Assorted mushhrooms</label>
           </div>
           <div>
-            <input type="checkbox" name="orders" value="spinach"/><label>Spinach</label>
+            <input type="checkbox" name="orders" value="spinach" onChange={handleBoxChange} checked={items.includes('spinach')}/><label>Spinach</label>
           </div>
           <h1>Total:{calculateTotal()}</h1>
       </React.Fragment>

9c76e55ae7829225593e3f9a80b384b7992ff903
lurbh <53887968+lurbh@users.noreply.github.com>
2024-03-27 12:55:23 +0800

    Taste Checking Completed

--- 18-taste-checking/18-taste-checking/src/Tastes.js
+++ 18-taste-checking/18-taste-checking/src/Tastes.js
@@ -4,15 +4,63 @@ export default function Tastes () {
 
     const [selectedTastes, setSelectedTastes] = useState([]);
 
-    return <React.Fragment>
-      <input type="checkbox" value='spicy' /><label>Spicy</label>
-      <input type="checkbox" value='sour' /><label>Sour</label>
-      <input type="checkbox" value="Sweet" /><label>Sweet</label>
+    const spicyOnClick = () =>{
+      if(!selectedTastes.includes('spicy'))
+      {
+        const cloneSelectedTastes = selectedTastes.slice();
+        cloneSelectedTastes.push('spicy');
+        setSelectedTastes(cloneSelectedTastes);
+      }
+      else
+      {
+        const cloneSelectedTastes = selectedTastes.slice();
+        const indexToRemove = cloneSelectedTastes.indexOf('spicy');
+        cloneSelectedTastes.splice(indexToRemove,1)
+        setSelectedTastes(cloneSelectedTastes);
+      }
+    }
 
+    const sourOnClick = () =>{
+      if(!selectedTastes.includes('sour'))
+      {
+        const cloneSelectedTastes = selectedTastes.slice();
+        cloneSelectedTastes.push('sour');
+        setSelectedTastes(cloneSelectedTastes);
+      }
+      else
+      {
+        const cloneSelectedTastes = selectedTastes.slice();
+        const indexToRemove = cloneSelectedTastes.indexOf('sour');
+        cloneSelectedTastes.splice(indexToRemove,1)
+        setSelectedTastes(cloneSelectedTastes);
+      }
+    }
+
+    const sweetOnClick = () =>{
+      if(!selectedTastes.includes('Sweet'))
+      {
+        const cloneSelectedTastes = selectedTastes.slice();
+        cloneSelectedTastes.push('Sweet');
+        setSelectedTastes(cloneSelectedTastes);
+      }
+      else
+      {
+        const cloneSelectedTastes = selectedTastes.slice();
+        const indexToRemove = cloneSelectedTastes.indexOf('Sweet');
+        cloneSelectedTastes.splice(indexToRemove,1)
+        setSelectedTastes(cloneSelectedTastes);
+      }
+    }
+
+    return <React.Fragment>
+      <input type="checkbox" value='spicy' checked={selectedTastes.includes('spicy')}/><label>Spicy</label>
+      <input type="checkbox" value='sour'  checked={selectedTastes.includes('sour')}/><label>Sour</label>
+      <input type="checkbox" value="Sweet"  checked={selectedTastes.includes('Sweet')}/><label>Sweet</label>
+    
       <div>
-        <button>Spicy</button>
-        <button>Sour</button>
-        <button>Sweet</button>
+        <button onClick={spicyOnClick}>Spicy</button>
+        <button onClick={sourOnClick}>Sour</button>
+        <button onClick={sweetOnClick}>Sweet</button>
       </div>
     </React.Fragment>
 }

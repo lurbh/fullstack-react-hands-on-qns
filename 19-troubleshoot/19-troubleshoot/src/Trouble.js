c59fee2d3ecdbd3e5f471b64d330f9d23d9d04d3
lurbh <53887968+lurbh@users.noreply.github.com>
2024-03-27 13:42:58 +0800

    Troubles\hooting Completed

--- 19-troubleshoot/19-troubleshoot/src/Trouble.js
+++ 19-troubleshoot/19-troubleshoot/src/Trouble.js
@@ -18,7 +18,7 @@ export default function Trouble() {
     const [newFaultName, setNewFaultName] = useState("");
     const [newTroubleType, setNewTroubleType] = useState("");
     const [newDescription, setNewDescription] = useState("");
-    const [newTags, setTags] = useState("");
+    const [newTags, setTags] = useState([]);
 
     const faultsContainer = [];
 
@@ -27,22 +27,40 @@ export default function Trouble() {
         for (let f of faults) {
         faultList.push(<div>
             <h5>{f.name}</h5>
-            <h6>Reported by: <span>f.name (f.email)</span></h6>
-            <h6>Trouble: <span>f.trouble_type</span></h6>
-            <h6>Tags:<span>{f.tags.join(',')}</span></h6>
+            <h6>Reported by: <span>{f.name} ({f.email})</span></h6>
+            <h6>Trouble: <span>{f.trouble_type}</span></h6>
+            <h6>Tags: <span>{f.tags.join(',')}</span></h6>
             <p>
                 {f.description}
             </p>
         </div>)
         }
+        return faultList;
     }
 
     function updateEmail (e) {
-        setNewFaultEmail(e.target.value);
+        setNewFaultEmail(e);
     }
   
     function updateCheckbox (e) {
-
+        if(e.target.name === 'trouble_type')
+        {
+          setNewTroubleType(e.target.value);
+        }
+        else if(e.target.name === 'tags')
+        {
+          const { value, checked } = e.target;
+          if (checked)
+            if(newTags.length  > 0)
+              setTags([...newTags, value]);
+            else
+              setTags([value]);
+          else
+          {
+            let tagarray = newTags.filter((item) => item !== value)
+              setTags(tagarray);
+          }
+        }
     }
 
     function addFault () {
@@ -58,7 +76,7 @@ export default function Trouble() {
         }
 
         // make a clone
-        let clone = faults;
+        let clone = faults.slice();
 
         // push into the clone
         clone.push(newFault);
@@ -83,28 +101,28 @@ export default function Trouble() {
           </div>
           <div>
             <label>Name</label>
-            <input type="text" name="email" value={newFaultEmail} onChange={(event)=> setNewFaultName(event.target.value)}/>
+            <input type="text" name="name" value={newFaultName} onChange={(event)=> setNewFaultName(event.target.value)}/>
           </div>
             <div>
             <label>Description</label>
-            <input type="text" name="description" value={newTroubleType} onChange={(event)=>setNewTroubleType(event.target.value)}/>
+            <input type="text" name="description" value={newDescription} onChange={(event)=>setNewDescription(event.target.value)}/>
           </div>
           <div>
             <label>Trouble Type</label>
 
-            <input type="radio" name="trouble_type" value="equipment-fault" onChange={updateCheckbox}/><label>Equipment Fault</label>
+            <input type="radio" name="trouble_type" value="equipment-fault" onChange={updateCheckbox} checked={newTroubleType==="equipment-fault"}/><label>Equipment Fault</label>
 
-             <input type="radio" name="trouble_type" value="building-defect" onChange={updateCheckbox}/><label>Building Defect</label>
+             <input type="radio" name="trouble_type" value="building-defect" onChange={updateCheckbox} checked={newTroubleType==="building-defect"}/><label>Building Defect</label>
 
-            <input type="radio" name="trouble_type" value="other-complaints" onChange={updateCheckbox}/><label>Others</label>
+            <input type="radio" name="trouble_type" value="other-complaints" onChange={updateCheckbox} checked={newTroubleType==="other-complaints"}/><label>Others</label>
           
           </div>
           <div>
             <label>Tags</label>
-            <input type="checkbox" name="tags" value="urgent" onChange={updateCheckbox}/><label>Urgent</label>
-            <input type="checkbox" name="tags" value="dangerous" onChange={updateCheckbox}/><label>Dangerous</label>
-            <input type="checkbox" name="tags" value="public-areas" onChange={updateCheckbox}/><label>Public Areas</label>
-            <input type="checkbox" name="tags" value="electrical" onChange={updateCheckbox}/><label>Electrical</label>
+            <input type="checkbox" name="tags" value="urgent" onChange={updateCheckbox} checked={newTags.includes('urgent')}/><label>Urgent</label>
+            <input type="checkbox" name="tags" value="dangerous" onChange={updateCheckbox} checked={newTags.includes('dangerous')}/><label>Dangerous</label>
+            <input type="checkbox" name="tags" value="public-areas" onChange={updateCheckbox} checked={newTags.includes('public-areas')}/><label>Public Areas</label>
+            <input type="checkbox" name="tags" value="electrical" onChange={updateCheckbox} checked={newTags.includes('electrical')}/><label>Electrical</label>
           </div>
           <button onClick={addFault}>Add Fault</button>
         </div>

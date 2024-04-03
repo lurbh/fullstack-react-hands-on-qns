26f126ef3da1610cb020bb7bac01fc058fed5e39
lurbh <53887968+lurbh@users.noreply.github.com>
2024-04-03 15:47:54 +0800

    tidy and reorder code handling

--- 17-add-and-update/17-add-and-update/src/Library.js
+++ 17-add-and-update/17-add-and-update/src/Library.js
@@ -27,39 +27,27 @@ export default function Movies() {
     const [index,setIndex] = useState(0);
     const [isEditing, setIsEditing] = useState(false); 
 
-    function getBook(id)
-    {
-        for(let b of books)
+    const handleInput = (e) => {
+        const {value, name} = e.target;
+        if(name === 'title')
+            setTitle(value)
+        else if(name === 'author')
+            setAuthor(value)
+        else if(name === 'genre')
+            setGenre(value)
+        else if(name === 'tags')
         {
-            if(b.id === id)
-                return b;
-        }
-        return null;
-    }
-
-    const handleTitle = (e) => {
-        setTitle(e.target.value)
-    }
-
-    const handleAuthor = (e) => {
-        setAuthor(e.target.value)
-    }
-
-    const handleGenre = (e) => {
-        setGenre(e.target.value)
-    }
-
-    const handleTags = (e) => {
-        const {value, checked} = e.target;
-        if(checked)
-            if(tagsList.length > 0)
-                setTagsList([...tagsList,value])
+            const {checked} = e.target;
+            if(checked)
+                if(tagsList.length > 0)
+                    setTagsList([...tagsList,value])
+                else
+                    setTagsList([value])
             else
-                setTagsList([value])
-        else
-        {
-            let tagsarray = tagsList.filter((t) => t !== value);
-            setTagsList(tagsarray);
+            {
+                let tagsarray = tagsList.filter((t) => t !== value);
+                setTagsList(tagsarray);
+            }
         }
     }
 
@@ -73,6 +61,7 @@ export default function Movies() {
         }
         const newbooks = [...books,newbook];
         setBooks(newbooks);
+
         setTitle(""); 
         setAuthor(""); 
         setGenre("");
@@ -92,32 +81,23 @@ export default function Movies() {
         let indexToEdit = books.findIndex((value) => {
             return value.id === index;
         });
-
         let clonedBooks = books.slice();
-
         clonedBooks.splice(indexToEdit,1,editedbook);
-
         setBooks(clonedBooks);
-        
 
-        const header = document.querySelector('#formHeader');
-        header.innerHTML = "Add Book";
-        const formButton = document.querySelector('#formButton');
-        formButton.innerHTML = "Add Book"
         setTitle(""); 
         setAuthor(""); 
         setGenre("");
         setTagsList([])
     }
 
-    const displayEditBook = (e) => {
-        setIndex(parseInt(e.target.value));
-        const header = document.querySelector('#formHeader');
-        header.innerHTML = "Edit Book";
-        const formButton = document.querySelector('#formButton');
-        formButton.innerHTML = "Edit Book"
+    const displayEditBook = (id) => {
+        setIndex(parseInt(id));
         setIsEditing(true);
-        const bookToEdit = getBook(parseInt(e.target.value));
+        const bookToEdit = books.find((value) => {
+            return value.id === index;
+        });
+
         setTitle(bookToEdit.title); 
         setAuthor(bookToEdit.author); 
         setGenre(bookToEdit.genre);
@@ -126,34 +106,33 @@ export default function Movies() {
 
     const cancelEditBook = () => {
         setIsEditing(false);
-        const header = document.querySelector('#formHeader');
-        header.innerHTML = "Add Book";
-        const formButton = document.querySelector('#formButton');
-        formButton.innerHTML = "Add Book"
         setTitle(""); 
         setAuthor(""); 
         setGenre("");
         setTagsList([]);
+    }
 
+    const DeleteBook = (id) => {
+        const indexToRemove = books.findIndex((value) => {
+            return value.id === index;
+        }); 
+        const clonedBooks = books.slice();
+        clonedBooks.splice(indexToRemove,1);
+        setBooks(clonedBooks);
     }
 
     const displayBooks = () => {
         const fragmentList = [];
         for(let b of books)
         {
-            let tagstring = "";
-            for(let t of b.tags)
-            {
-                tagstring = tagstring + t + ", ";
-            }
-            tagstring = tagstring.slice(0,-2);
             const fragment = (<>
                 <div>
                     <h2>Title: {b.title}</h2>
                     <h3>Author: {b.author}</h3>
                     <h4>Genre: {b.genre}</h4>
-                    <h5>Tags: {tagstring}</h5>
-                    <button onClick={displayEditBook} value={b.id}>Edit</button>
+                    <h5>Tags: {b.tags.join(", ")}</h5>
+                    <button onClick={() => displayEditBook(b.id)} value={b.id}>Edit</button>
+                    <button onClick={() => DeleteBook(b.id)} value={b.id}>Delete</button>
                 </div>
             </>)
             fragmentList.push(fragment);
@@ -161,39 +140,37 @@ export default function Movies() {
         return fragmentList;
     }
 
-    const handleClick = isEditing ? updateBook : addBook; 
-
     return <React.Fragment>
-        <div class="container">
-            <div class="col">
+        <div className="container">
+            <div className="col">
                 {displayBooks()}
             </div>
 
-            <div class="col">
+            <div className="col">
 
-            <h3 id="formHeader">Add Book</h3>
+            <h3>{isEditing ? "Edit Book" : "Add Book"}</h3>
             <div>
                 <label>Title:</label>
-                <input type="text" name="title" id='title' onChange={handleTitle} value={title}/>
+                <input type="text" name="title" id='title' onChange={handleInput} value={title}/>
             </div>
             <div>
                 <label>Author</label>
-                <input type="text" name="author" id='author' onChange={handleAuthor} value={author}/>
+                <input type="text" name="author" id='author' onChange={handleInput} value={author}/>
             </div>
             <div>
                 <label>Genre</label>
-                <input type="radio" name="genre" id="fantasy" value="fantasy" onChange={handleGenre} checked={genre === "fantasy"}/><label>Fantasy</label>
-                <input type="radio" name="genre" id="science-fiction" value="science-fiction" onChange={handleGenre} checked={genre === "science-fiction"}/><label>Science Fiction</label>
-                <input type="radio" name="genre" id="romance" value="romance" onChange={handleGenre} checked={genre === "romance"}/><label>Romance</label>
+                <input type="radio" name="genre" id="fantasy" value="fantasy" onChange={handleInput} checked={genre === "fantasy"}/><label>Fantasy</label>
+                <input type="radio" name="genre" id="science-fiction" value="science-fiction" onChange={handleInput} checked={genre === "science-fiction"}/><label>Science Fiction</label>
+                <input type="radio" name="genre" id="romance" value="romance" onChange={handleInput} checked={genre === "romance"}/><label>Romance</label>
             </div>
             <div>
                 <label>Tags</label>
-                <input type="checkbox" name="tags" id="classic" value="classic" onChange={handleTags} checked={tagsList.includes("classic")}/><label>Classic</label>
-                <input type="checkbox" name="tags" id="doorstopper" value="doorstopper" onChange={handleTags} checked={tagsList.includes("doorstopper")}/><label>Doorstopper</label>
-                <input type="checkbox" name="tags" id="futuristic" value="futuristic" onChange={handleTags} checked={tagsList.includes("futuristic")}/><label>futuristic</label>
-                <input type="checkbox" name="tags" id="trilogy" value="trilogy" onChange={handleTags} checked={tagsList.includes("trilogy")}/><label>trilogy</label>
+                <input type="checkbox" name="tags" id="classic" value="classic" onChange={handleInput} checked={tagsList.includes("classic")}/><label>Classic</label>
+                <input type="checkbox" name="tags" id="doorstopper" value="doorstopper" onChange={handleInput} checked={tagsList.includes("doorstopper")}/><label>Doorstopper</label>
+                <input type="checkbox" name="tags" id="futuristic" value="futuristic" onChange={handleInput} checked={tagsList.includes("futuristic")}/><label>futuristic</label>
+                <input type="checkbox" name="tags" id="trilogy" value="trilogy" onChange={handleInput} checked={tagsList.includes("trilogy")}/><label>trilogy</label>
             </div>
-            <button onClick={handleClick} id="formButton">Add Book</button>
+            <button onClick={isEditing ? updateBook : addBook} id="formButton">{isEditing ? "Update Book" : "Add Book"}</button>
             <button onClick={cancelEditBook}>Cancel</button>
             </div>
         </div>
